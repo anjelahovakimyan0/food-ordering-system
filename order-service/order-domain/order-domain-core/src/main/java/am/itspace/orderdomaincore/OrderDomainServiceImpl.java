3153b78e0d5fa824f8b1073eccfca7ac833ab521
Anjela Hovakimyan <hovakimyan.0anjela@gmail.com>
2024-07-02 11:52:27 +0300

    Unit tests, create order flow

--- order-service/order-domain/order-domain-core/src/main/java/am/itspace/orderdomaincore/OrderDomainServiceImpl.java
+++ order-service/order-domain/order-domain-core/src/main/java/am/itspace/orderdomaincore/OrderDomainServiceImpl.java
@@ -56,7 +56,8 @@ public class OrderDomainServiceImpl implements OrderDomainService {
 
     private void validateRestaurant(Restaurant restaurant) {
         if (!restaurant.isActive()) {
-            throw new OrderDomainException("Restaurant with id " + restaurant.getId().getValue()
+            throw new OrderDomainException(
+                    "Restaurant with id " + restaurant.getId().getValue()
                     + " is currently not active!");
         }
     }
